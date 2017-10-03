88879374dcb8b9ed7371400784b8939df830040c
Joshua Foo <cozyazure@gmail.com>
2017-10-04 02:05:50 +0800

    done with loading async

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,17 +1,171 @@
 import React from 'react';
+import Autosuggest from 'react-autosuggest';
 import './Searchbar.css';
 
-const Searchbar = (props) => {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="four columns">&nbsp;</div>
-                <div className="four columns searchbar">
-                    <input className="u-full-width" type="text" placeholder="Search anything.."/>
-                </div>
-                <div className="four columns">&nbsp;</div>
-            </div>
-        </div>
-    )
+const languages = [
+    {
+        name: 'C',
+        year: 1972
+    },
+    {
+        name: 'C#',
+        year: 2000
+    },
+    {
+        name: 'C++',
+        year: 1983
+    },
+    {
+        name: 'Clojure',
+        year: 2007
+    },
+    {
+        name: 'Elm',
+        year: 2012
+    },
+    {
+        name: 'Go',
+        year: 2009
+    },
+    {
+        name: 'Haskell',
+        year: 1990
+    },
+    {
+        name: 'Java',
+        year: 1995
+    },
+    {
+        name: 'Javascript',
+        year: 1995
+    },
+    {
+        name: 'Perl',
+        year: 1987
+    },
+    {
+        name: 'PHP',
+        year: 1995
+    },
+    {
+        name: 'Python',
+        year: 1991
+    },
+    {
+        name: 'Ruby',
+        year: 1995
+    },
+    {
+        name: 'Scala',
+        year: 2003
+    }
+];
+
+function getMatchingLanguages(value) {
+    const escapedValue = escapeRegexCharacters(value.trim());
+
+    if (escapedValue === '') {
+        return [];
+    }
+
+    const regex = new RegExp('^' + escapedValue, 'i');
+
+    return languages.filter(language => regex.test(language.name));
 }
+
+function escapeRegexCharacters(str) {
+    return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+function getSuggestionValue(suggestion) {
+    return suggestion.name;
+}
+
+function renderSuggestion(suggestion) {
+    return (
+        <span>{suggestion.name}</span>
+    );
+}
+
+class Searchbar extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            value: '',
+            suggestions: [],
+            isLoading: false
+        };
+
+        this.lastRequestId = null;
+    }
+
+    loadSuggestions(value) {
+        // Cancel the previous request
+        if (this.lastRequestId !== null) {
+            clearTimeout(this.lastRequestId);
+        }
+
+        this.setState({
+            isLoading: true
+        });
+
+        // Fake request
+        this.lastRequestId = setTimeout(() => {
+            this.setState({
+                isLoading: false,
+                suggestions: getMatchingLanguages(value)
+            });
+        }, 1000);
+    }
+
+    onChange = (event, {newValue}) => {
+        this.setState({
+            value: newValue
+        });
+    };
+
+    onSuggestionsFetchRequested = ({value}) => {
+        this.loadSuggestions(value);
+    };
+
+    onSuggestionsClearRequested = () => {
+        this.setState({
+            suggestions: []
+        });
+    };
+
+    render() {
+        const {value, suggestions, isLoading} = this.state;
+        const inputProps = {
+            placeholder: "Type 'c'",
+            value,
+            onChange: this.onChange
+        };
+        const status = (isLoading ? 'Loading...' : 'Type to load suggestions:');
+
+        return (
+            <div className="container">
+
+                <div className="row">
+                    <div className="two columns">&nbsp;</div>
+                    <div className="eight columns">
+                        <div className="status">
+                            {status}
+                        </div>
+                        <Autosuggest
+                            suggestions={suggestions}
+                            onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+                            onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                            getSuggestionValue={getSuggestionValue}
+                            renderSuggestion={renderSuggestion}
+                            inputProps={inputProps}/>
+                    </div>
+                    <div className="two columns">&nbsp;</div>
+                </div>
+            </div>
+        );
+    }
+}
+
 export default Searchbar;
